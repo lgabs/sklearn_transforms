2ce0c00014139513fce75d75cbe2650d829faff5
lgabs <luan.fernandes@redealumni.com>
2020-08-21 21:01:01 -0300

    add add_features class

--- my_custom_sklearn_transforms/sklearn_transformers.py
+++ my_custom_sklearn_transforms/sklearn_transformers.py
@@ -14,3 +14,19 @@ class DropColumns(BaseEstimator, TransformerMixin):
         data = X.copy()
         # Retornamos um novo dataframe sem as colunas indesejadas
         return data.drop(labels=self.columns, axis='columns')
+
+class AddFeatures(BaseEstimator, TransformerMixin):
+    def __init__(self):
+        pass
+
+    def fit(self, X, y=None):
+        return self
+    
+    def transform(self, X):
+        # Primeiro realizamos a cópia do dataframe 'X' de entrada
+        df = X.copy()
+        df["avg_humanas"] = df[["NOTA_DE", "NOTA_EM", "NOTA_GO", "INGLES"]].mean(axis=1)
+        df["reprovacoes_total"] = df[["REPROVACOES_DE", "REPROVACOES_EM", "REPROVACOES_MF", "REPROVACOES_GO"]].mean(axis=1)
+        df["study_volume"] = df["H_AULA_PRES"] * df["TAREFAS_ONLINE"]
+        # Retornamos um novo dataframe sem as colunas indesejadas
+        return df
